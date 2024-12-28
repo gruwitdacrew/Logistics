835df0872bb09975fbe3bb6cc02a85c382b26a27
Arseniy <arsen190104@gmail.com>
2024-12-28 21:16:11 +0700

    Некоторые изменения

--- Logistics/Services/Utils/TokenGenerator/TokenGeneratorConfiguration.cs
+++ Logistics/Services/Utils/TokenGenerator/TokenGeneratorConfiguration.cs
@@ -3,12 +3,12 @@
     public class TokenGeneratorConfiguration
     {
         public string AccessTokenSecret { get; set; } = Environment.GetEnvironmentVariable("ACCESS_TOKEN_SECRET");
-        public int AccessTokenExpirationSeconds { get; set; }
-        public int RefreshTokenExpirationHours { get; set; }
-        public int ApproveEmailTokenExpirationMinutes { get; set; }
-        public int ResetPasswordTokenExpirationMinutes { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
+        public int AccessTokenExpirationSeconds { get; set; } = 600;
+        public int RefreshTokenExpirationHours { get; set; } = 24;
+        public int ApproveEmailTokenExpirationMinutes { get; set; } = 15;
+        public int ResetPasswordTokenExpirationMinutes { get; set; } = 15;
+        public string Issuer { get; set; } = "Logistics";
+        public string Audience { get; set; } = "Logistics/api";
 
     }
 }
